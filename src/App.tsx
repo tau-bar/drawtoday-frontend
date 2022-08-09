ca1ea013185d2f57629d059fe5289187eb92152c
tau-bar <taufiq.b.ar@gmail.com>
2022-08-09 23:08:25 +0800

    Add Browse Page and add navigation between pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { Brightness4, Brightness7 } from "@mui/icons-material";
-import { CssBaseline, IconButton } from "@mui/material";
+import {
+  Brightness4,
+  Brightness7,
+  Brush,
+  DynamicFeed,
+} from "@mui/icons-material";
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  CssBaseline,
+  IconButton,
+  Paper,
+} from "@mui/material";
 import { cyan, deepOrange, grey, orange } from "@mui/material/colors";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./App.scss";
 import { THEME } from "./config/constants";
+import BrowsePage from "./pages/browse-page/BrowsePage";
 import DrawPage from "./pages/draw-page/DrawPage";
 import { toggleTheme } from "./redux-saga/actions";
 import { RootState } from "./redux-saga/reducers/rootReducer";
@@ -40,19 +52,38 @@ const App = () => {
     typography: {},
   });
 
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const pages = [<DrawPage />, <BrowsePage />];
+
   return (
     <ThemeProvider theme={muiTheme}>
       <CssBaseline />
       <div className="App">
         <IconButton
+          sx={{ ml: 1, position: "fixed", right: 10, top: 20 }}
           className="ThemeToggle"
-          sx={{ ml: 1 }}
           onClick={() => setTimeout(() => dispatch(toggleTheme()), 200)}
           color="inherit"
         >
           {mode === THEME.DARK ? <Brightness7 /> : <Brightness4 />}
         </IconButton>
-        <DrawPage />
+        {pages[currentPage]}
+        <Paper
+          sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+          elevation={3}
+        >
+          <BottomNavigation
+            showLabels
+            value={currentPage}
+            onChange={(_, newValue) => {
+              setCurrentPage(newValue);
+            }}
+          >
+            <BottomNavigationAction label="Draw" icon={<Brush />} />
+            <BottomNavigationAction label="Browse" icon={<DynamicFeed />} />
+          </BottomNavigation>
+        </Paper>
       </div>
     </ThemeProvider>
   );
---
+++ src/pages/browse-page/BrowsePage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./BrowsePage.scss";
+
+const BrowsePage = ({ ...other }) => {
+  return <div className="BrowsePage">This is the browse page</div>;
+};
+
+export default BrowsePage;
