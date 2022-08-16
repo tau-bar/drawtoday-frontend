7dd8cfa055f66e718c2123188bf7bace12622227
tau-bar <taufiq.b.ar@gmail.com>
2022-08-17 00:27:21 +0800

    Change touch action on canvas

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -58,7 +58,7 @@ const PostCard: FC<PostProps> = ({ post, ...other }) => {
 					<p>{post.word}</p>
 				</Container>
 				<CanvasDraw
-					style={{ touchAction: "none" }}
+					style={{ touchAction: "auto" }}
 					canvasHeight={300}
 					canvasWidth={300}
 					disabled
